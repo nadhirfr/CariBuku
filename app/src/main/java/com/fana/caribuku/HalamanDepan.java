3c6e71e7b17fa131dfc7b9397c65f28a17820a30
najib <kang.najiib@gmail.com>
2016-05-25 15:10:02 +0700

    fahrul edit scroll

--- app/src/main/java/com/fana/caribuku/HalamanDepan.java
+++ app/src/main/java/com/fana/caribuku/HalamanDepan.java
@@ -84,8 +84,6 @@ public class HalamanDepan extends AppCompatActivity
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
